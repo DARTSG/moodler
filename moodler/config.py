ec7e388bb1f9b0adea2fa3a831672d91f7d23d66
maxandron <ron.suhodrev@gmail.com>
2020-06-02 11:12:38 +0800

    use environs instead of hard-conded config

--- moodler/config.py
+++ moodler/config.py
@@ -1,69 +1,11 @@
-# Ivy
-# TOKEN = '53e2cd85d463774b6b4dc67e485ca61e'
-# URL = 'http://192.168.10.158'
+from environs import Env
 
-# Stagmont camp
-TOKEN = '6932c6a583c1200d303d97109e4d7fec'
-URL = 'http://192.168.1.55'
+env = Env()
+env.read_env()
+
+TOKEN = env('TOKEN')
+URL = env.url('URL')
 LOGIN_PAGE = 'login/index.php'
 
 # List of names of students to not grade
-STUDENTS_TO_IGNORE = {
-    147: "John Teoh",
-    # 187: "Wen Zong Teo"
-}
-
-USER_MAP = {
-    155: 'Chee Kwan Yong',
-    156: 'Toh Lee Yang',
-    188: 'Rayton Ng',
-    198: 'Brandon Lee Jia Le',
-    199: 'Chee Han Ooi',
-    200: 'Cheng Kiat Koh',
-    201: 'Chng Wei Cheng chng',
-    202: 'Clement Leow Seng Woon',
-    203: 'Darren Wong Ding Jie',
-    204: 'Dennis Poh Yong Hup',
-    205: 'Elton Ong Jun Wen',
-    206: 'Eugene Chew Ee Kin',
-    207: 'Harish S/O Balamurugan',
-    208: 'Harwinder Singh Dhillon',
-    209: 'Isaac Choo Junhui',
-    210: 'Jarren Liow Jie Ren',
-    211: 'Jason Tay',
-    212: 'Jeffrey Oon Shao Xian',
-    213: 'Jing Hong Chew',
-    214: 'Joe Ong Bin Han',
-    215: 'Joshua Lee Jun Yi',
-    216: 'Jun Min Teo',
-    217: 'Jun Rui Lim',
-    218: 'Justin Soh Jun Hao',
-    219: 'Kai Boon Ong',
-    220: 'Kennard Chua Guo Xian',
-    221: 'Kian Yang Quah',
-    222: 'Kian Yong Ng',
-    223: 'Louis Neo Yu Xiang',
-    224: 'Ming Hui Teng',
-    225: 'Nicholas Fong Zhi Qian',
-    226: 'Qin Fa See',
-    227: 'Raymous Ang Jin Yuan',
-    228: 'Reuben Ong Zhi Kai',
-    229: 'Ronnie Tang Jun Wen',
-    230: 'Shaun Tan',
-    231: 'Shui Yuan Law',
-    232: 'Shun Han How',
-    233: 'Siong Yong Tan',
-    234: 'Suan Hong Lee',
-    235: 'Vernon Tay Jun Feng',
-    236: 'Wai Qun Low',
-    237: 'Wei Liang Ng',
-    238: 'Wei Xuan Wong',
-    239: 'Xin Wei Chan',
-    240: 'Yan Han Sim',
-    241: 'Yongze Zhu',
-    242: 'Zames Li Chengyang',
-    243: 'Zheng Jie Lum',
-    244: 'Aqil Ahmad Subahan',
-    245: 'Mike Mai',
-    246: 'Wei SIang Leow'
-}
+STUDENTS_TO_IGNORE = env.dict('STUDENTS_TO_IGNORE', subcast_key=int, default={})
