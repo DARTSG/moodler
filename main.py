22498f22aba08a7c9d0c4681a4d7d4436dc373cd
maxandron <ron.suhodrev@gmail.com>
2020-06-12 16:16:29 +0800

    Delete __init__ from root directory
    the root directory is not part of the package and it confused my
    language server

--- main.py
+++ main.py
@@ -27,8 +27,7 @@ import argparse
 import logging
 from pprint import pprint
 
-from moodler.moodle import ungraded_submissions, export_feedbacks, \
-    export_all
+from moodler.moodle import export_all, export_feedbacks, ungraded_submissions
 from moodler.students import get_students
 
 logger = logging.getLogger(__name__)
