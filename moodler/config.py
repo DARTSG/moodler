f72579fb77049d52146aa5ea32ca3d18bfa07498
Almog Carmi <almogcarmi@gmail.com>
2020-06-16 10:38:42 +0800

    Added fixup so now Moodler will use the new grades export logic.

--- moodler/config.py
+++ moodler/config.py
@@ -7,5 +7,8 @@ TOKEN = env('TOKEN')
 URL = env('URL')
 LOGIN_PAGE = 'login/index.php'
 
+MOODLE_USERNAME = env('MOODLE_USERNAME')
+MOODLE_PASSWORD = env('MOODLE_PASSWORD')
+
 # List of names of students to not grade
 STUDENTS_TO_IGNORE = env.dict('STUDENTS_TO_IGNORE', subcast_key=int, default={})
