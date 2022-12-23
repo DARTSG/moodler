964ebd08e18ac922ed2bc4645ba2423a14c96e0d
Ron Suhodrev <ron.suhodrev@gmail.com>
2022-12-23 14:19:13 +0800

    chore: add typing to glossary api responses

--- moodler/glossary.py
+++ moodler/glossary.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Union
+from typing import TypedDict
 
 from moodler.moodle_api import call_moodle_api
 from moodler.moodle_exception import MoodlerException
@@ -16,15 +16,43 @@ class NoGlossaryEntry(GlossaryException):
     pass
 
 
-def mod_glossary_get_entries_by_search(
-    glossary_id: int, query: str
-) -> Dict[str, Union[int, List, Dict]]:
+class GlossaryEntry(TypedDict):
+    id: int
+    glossaryid: int
+    userid: int
+    userfullname: str
+    userpictureurl: str
+    concept: str
+    definition: str
+    definitionformat: int
+    definitiontrust: bool
+    attachment: bool
+    attachments: list[object]
+    timecreated: int
+    timemodified: int
+    teacherentry: bool
+    sourceglossaryid: int
+    usedynalink: bool
+    casesensitive: bool
+    fullmatch: bool
+    approved: bool
+    tags: list[object]
+
+
+class GlossaryEntries(TypedDict):
+    count: int
+    entries: list[GlossaryEntry]
+    ratinginfo: dict
+    warnings: list[dict]
+
+
+def mod_glossary_get_entries_by_search(glossary_id: int, query: str) -> GlossaryEntries:
     """
     Returns the list of entries containing the search term in query
     :param glossary_id: The ID of the glossary, which could be obtained using mod_glossary_get_glossaries_by_courses function
     :param query: The string to search within the given glossary
     """
-    response = call_moodle_api(
+    response: GlossaryEntries = call_moodle_api(
         "mod_glossary_get_entries_by_search", id=glossary_id, query=query
     )
 
@@ -39,13 +67,58 @@ def mod_glossary_get_entries_by_search(
     return response
 
 
-def mod_glossary_get_glossaries_by_courses(courseid: int) -> Dict[str, List]:
+class Glossary(TypedDict):
+    id: int
+    coursemodule: int
+    course: int
+    name: str
+    intro: str
+    introformat: int
+    introfiles: list[object]
+    section: int
+    visible: bool
+    groupmode: int
+    groupingid: int
+    lang: str
+    allowduplicatedentries: int
+    displayformat: str
+    mainglossary: int
+    showspecial: int
+    showalphabet: int
+    showall: int
+    allowcomments: int
+    allowprintview: int
+    usedynalink: int
+    defaultapproval: int
+    approvaldisplayformat: str
+    globalglossary: int
+    entbypage: int
+    editalways: int
+    rsstype: int
+    rssarticles: int
+    assessed: int
+    assesstimestart: int
+    assesstimefinish: int
+    scale: int
+    timecreated: int
+    timemodified: int
+    completionentries: int
+    browsemodes: list[str]
+    canaddentry: int
+
+
+class Glossaries(TypedDict):
+    glossaries: list[Glossary]
+    warnings: list[dict]
+
+
+def mod_glossary_get_glossaries_by_courses(courseid: int) -> Glossaries:
     """
     Returns a list of glossaries for the given course in a dict
     mapping glossary id to glossary
     e.g. [{id: 1, 'name': 'Test Files', ...}]
     """
-    response = call_moodle_api(
+    response: Glossaries = call_moodle_api(
         "mod_glossary_get_glossaries_by_courses", courseids=[courseid]
     )
 
