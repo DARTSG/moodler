040271e9e189a9d6c7ee2c73fc75ba07de26a35d
Ang Pei Hao <ang_pei_hao@dart.com.sg>
2024-09-23 16:08:16 +0800

    Add report.py

---
+++ moodler/report.py
@@ -0,0 +1,8 @@
+from moodler.moodle_api import call_moodle_api
+
+def gradereport_user_get_grade_items(courseid: int):
+    """
+    Get the grade report of all the users by course id
+    """
+    response = call_moodle_api("gradereport_user_get_grade_items", courseid=courseid)
+    return response
