1f0a424ba6a9e025b54422b0daed6c0ae3656781
Ang Pei Hao <ang_pei_hao@dart.com.sg>
2024-10-23 10:57:30 +0800

    Validate gradereport_user_get_grade_items response

--- moodler/report.py
+++ moodler/report.py
@@ -1,9 +1,23 @@
 from moodler.moodle_api import call_moodle_api
 
 
+class GradeReportException(Exception):
+    pass
+
+
 def gradereport_user_get_grade_items(courseid: int):
     """
     Get the grade report of all the users by course id
     """
-    response = call_moodle_api("gradereport_user_get_grade_items", courseid=courseid)
-    return response
+    gradereport = call_moodle_api(
+        "gradereport_user_get_grade_items", courseid=courseid
+    )
+
+    # Validate grade report
+    if "usergrades" not in gradereport or "warnings" not in gradereport:
+        raise GradeReportException("Incorrect grade report format.")
+
+    if gradereport["warnings"]:
+        raise GradeReportException(gradereport["warnings"])
+
+    return gradereport
